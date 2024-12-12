0d5b58ad44ffcf4de6fa4003aea83d614ff763f6
Flotter Totte <flottertotte@emeneo.com>
2024-12-12 14:42:20 +0100

    Update provider.php

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/** This code is based on plugin customfield_checkbox  
+/** This code is based on plugin customfield_checkbox Privacy Subsystem implementation by:  
 * @copyright 2018 Daniel Neis Araujo <danielneis@gmail.com>
 */
 
